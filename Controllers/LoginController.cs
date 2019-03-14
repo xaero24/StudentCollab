6db463252aa55f292e3a5952e98061aea75b2adc
tzvip <tzvi23@gmail.com>
2019-03-14 10:56:06 +0200

    First Commit

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using StudentCollab.Models;
+
+namespace StudentCollab.Controllers
+{
+    public class LoginController : Controller
+    {
+        public ActionResult Login()
+        {
+            return View(new User());
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact(User getUsr)
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View(getUsr);
+        }
+
+        public ActionResult Submit()
+        {
+            User usr = new User();
+            usr.UserName = Request.Form["username"];
+            usr.Password = Request.Form["pass"]; //same name
+
+            if (!(usr.UserName == null || usr.Password == null)) return RedirectToAction("Contact", usr);
+            else return RedirectToAction("Login");
+
+
+
+        }
+    }
+}
